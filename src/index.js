f0249b43dc8e95e4020008318df9a7d1a7a7500f
Soyo265 <soyokaze265@gmail.com>
2020-10-02 16:55:07 +0500

    feat: complete typical arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array && array instanceof Array && array.length !== 0){
+        let resultArray = array;
+        return resultArray.sort((a, b) => a - b)[0];
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array instanceof Array && array.length !== 0){
+        let resultArray = array;
+        return resultArray.sort((a, b) => a - b)[array.length - 1];
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array && array instanceof Array && array.length !== 0){
+        return array.reduce((sum, val) => sum += val) / array.length;
+    } else {
+        return 0;
+    }
 }
